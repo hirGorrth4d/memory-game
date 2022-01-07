15f462b6e2986bb7f7ccf463e7c6cf91b72437f6
sonovdragons <cagliolom@gmail.com>
2022-01-07 12:01:41 -0300

    memory game

---
+++ app.js
@@ -0,0 +1,124 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    //card options
+    const cardArray =[
+        {
+            name: 'cheeseburger',
+            img: 'img/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'img/cheeseburger.png'
+        },
+        {
+            name: 'fries',
+            img: 'img/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'img/fries.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'img/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'img/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'img/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'img/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'img/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'img/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'img/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'img/pizza.png'
+        }
+
+    ]
+
+    cardArray.sort(() => 0.5 - Math.random())
+    const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenId= []
+    let cardsWon = []
+
+    function createBoard(){
+        for (let i =0; i < cardArray.length; i++){
+            let card = document.createElement('img')
+            card.setAttribute('src', 'img/blank.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    //check matches
+
+    function checkForMatch(){
+        let cards = document.querySelectorAll('img')
+        let optionOneId = cardsChosenId[0]
+        let optionTwoId = cardsChosenId[1]
+
+        if (optionOneId == optionTwoId){
+            
+            cards[optionOneId].setAttribute('src','img/blank.png')
+            cards[optionTwoId].setAttribute('src','img/blank.png')
+            alert('same image')
+            console.log('same image')
+
+        } else if (cardsChosen[0] === cardsChosen[1]){
+            alert('You found a match')
+            console.log('found a match')
+            cards[optionOneId].setAttribute('src', 'img/white.png')
+            cards[optionTwoId].setAttribute('src', 'img/white.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'img/blank.png')
+            cards[optionTwoId].setAttribute('src', 'img/blank.png')
+            console.log('try again')
+            alert('sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length/2){
+            resultDisplay.textContent = 'encontraste todo'
+        }
+    }
+
+    //flipcard
+
+    function flipCard(){
+        let cardId= this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2){
+            setTimeout(checkForMatch, 200)
+        }
+    }
+
+
+
+    createBoard();
+
+})
---
+++ readme.md
@@ -0,0 +1,2 @@
+#juego de memoria
+#codigo de ania Kubow
